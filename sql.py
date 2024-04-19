475d9edfc83792d9475dd7629e1fb2779fc39b51
TogiHo <vlamarval@gmail.com>
2024-04-19 15:27:11 +0400

    test create table

---
+++ sql.py
@@ -0,0 +1,15 @@
+import sqlite3 as sql
+
+def create_database(db_name, table_name):
+    # Подключение к базе данных или создание новой, если её нет
+    conn = sql.connect(db_name)
+    c = conn.cursor()
+    
+    # Создание таблицы в базе данных
+    c.execute(f"CREATE TABLE {table_name}(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)")
+    
+    # Сохранение изменений и закрытие соединения
+    conn.commit()
+    conn.close()
+
+#create_database("database.db", "table_name")
