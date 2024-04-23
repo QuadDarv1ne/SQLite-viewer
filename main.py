a5b43a56fdc42b18ad35ad554db98bc7c12d3139
TogiHo <vlamarval@gmail.com>
2024-04-23 16:04:48 +0400

    Fix non uodate when db create

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main(page: ft.Page):
         selected_db.value = e.files[0].path if e.files else None
         global dbPATH
         dbPATH = selected_db.value if selected_db.value else dbPATH
+        #print(dbPATH)
         load_data(dbPATH) if dbPATH else None
             
     
@@ -26,6 +27,7 @@ def main(page: ft.Page):
         #print(e.control.data)
         sq.del_row(dbPATH, e.control.data, sq.get_columns(dbPATH))
     def load_data(dbpath):
+        #print(dbpath)
         columns = list(map(lambda x: ft.DataColumn(ft.Text(x)), sq.get_columns(dbPATH)))
         columns.append(ft.DataColumn(ft.Text("Действия")))
         rows = []
@@ -62,20 +64,27 @@ def main(page: ft.Page):
             sqlrequestColumns = "(" + sqlrequestColumns + ")"
         sq.create_database(dbPATH, tbNAME, sqlrequestColumns)
         create_database_alert.open = False
+        load_data(dbPATH)
         page.update()
+
+
     def create_database(e):
         page.dialog = create_database_alert
         create_database_alert.open = True
         page.update()
+
+
     def close_dlg_add_row(e):
         add_row_alert.content.clean()
         add_row_alert.open = False
         page.update()
+
     
     def close_dlg_add_database(e):
         #create_database_alert.content.clean()
         create_database_alert.open = False
         page.update()
+        
     
     def update_table_with_new_row(e):
         sq.add_row(dbPATH, tuple([i.value for i in addFIELDS]))
